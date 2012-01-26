618b57e75ed29bba4ee83c14fbcdecc25486b630
Michal Maruska <mmaruska@gmail.com>
2012-01-26 09:56:35 +0100

    isolate common functions into  Share dir.

--- Makefile
+++ Makefile
@@ -1,19 +1,27 @@
 
 
+SHAREDIR=/usr/share/build-system/
 INSTALL=install
 BIN_INSTALL_DIR = /usr/bin
 
 BINFILES=$(wildcard bin/*)
+SHARED_FILES=$(wildcard share/*)
 
 all:
 	echo ""
 
 install: install-zsh
 	$(INSTALL) -v -D --directory $(DESTDIR)$(BIN_INSTALL_DIR)
-	for p in $(BINFILES); do \
+	for p in $(SHARED_FILES); do \
 	  $(INSTALL) -v -m 555 $$p $(DESTDIR)$(BIN_INSTALL_DIR) ; \
 	done
 
+
+install-functions:
+	$(INSTALL) -v -D --directory $(DESTDIR)$(SHAREDIR)
+	for p in $(BINFILES); do \
+	  $(INSTALL) -v -m 555 $$p $(DESTDIR)$(SHAREDIR) ; \
+	done
 
 
 # zsh infrastructure to better use the provided commands!
---
+++ share/functions.sh
@@ -0,0 +1,65 @@
+#! /usr/bin/zsh
+
+# Common functions for the scripts
+
+
+
+# I want to detect a current tag & reuse it, ie. not recreate it.
+
+## return 0 & set the variables if successul.
+# variables:
+
+# VERSION
+# DISTRO
+# GIT_OFFSET
+current_tag()
+{
+    set +e
+# git describe --all -> HEAD or master.
+# I only want tags
+    description=$(git describe --tags)
+    status=$?
+    set -e
+
+    # if no tags at all -> exit, or ?
+    if [ $status ne 0 ]
+    then
+	if git tag -l |grep .;
+	then
+	    cecho red "not _past_ Git tag. But other tags present"
+	    if [ $FORCE != "y" ]
+	    then
+		:
+	    else
+		exit 1
+	    fi
+	else
+	    return 1
+	fi
+    else
+
+    # 2 possibilities:
+    # Either only the tag (possibly name/version),
+    # or name/version-offset-g{hash}
+    #
+	if [[ $description =~ "^(.*)/(.*)-([[:digit]]+)-g[[:alnum:]]*$" ]]
+	then
+	    DISTRO=match[1]
+	    VERSION=match[2]
+	    GIT_OFFSET=match[3]
+	    # ${${description%-g*}#*-}
+
+	elif [[ $description =~ "^(.*)/(.*)$" ]]
+	then
+	    DISTRO=match[1]
+	    VERSION=match[2]
+	    #VERSION=${description#*/}
+	    #DISTRO=${description%/*}
+	else
+	    exit
+	fi
+    fi
+
+    cecho yellow $VERSION $offset $hash >&2
+}
+
