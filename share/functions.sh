03001ed50610c1106403c02c8953fd57abf98423
Michal Maruska <mmaruska@gmail.com>
2013-08-09 02:19:45 +0200

    snap:  migrate to getopt
    Change-Id: I224c15c328fa60ded5163976c1bee224a9840ca2

--- share/functions.sh
+++ share/functions.sh
@@ -2,6 +2,13 @@
 
 # Common functions for the scripts
 
+check_getopt()
+{
+    if getopt -T; then # should test (( $? = -4 ))
+	echo "incompatible  getopt(1) installed. Abort"
+	exit -1
+    fi
+}
 
 possibly_trace()
 {
